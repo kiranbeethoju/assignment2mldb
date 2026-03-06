98058740f5b10bdbcc4e76d0c6ad8dd12d1a45d3
kiranbeethoju <33856173+kiranbeethoju@users.noreply.github.com>
2026-03-06 18:59:07 +0530

    Update run_all.py

--- run_all.py
+++ run_all.py
@@ -2,22 +2,18 @@ import os
 
 print("Running all questions...\n")
 
-print("="*60)
 print("QUESTION 1: k-Grams")
-print("="*60)
+
 os.system("python3 q1_kgrams.py")
 
-print("\n" + "="*60)
 print("QUESTION 2: Min-Hashing")
-print("="*60)
+
 os.system("python3 q2_minhash.py")
 
-print("\n" + "="*60)
 print("QUESTION 3: LSH")
-print("="*60)
+
 os.system("python3 q3_lsh.py")
 
-print("\n" + "="*60)
 print("QUESTION 4 & 5: MovieLens")
-print("="*60)
+
 os.system("python3 q4_q5_movielens.py")
